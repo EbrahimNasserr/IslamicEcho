b5a7c6b2b6f8ec3ffdc010e443b97150b19810e8
Ebrahim Nasser <ebrahimm.nasser@gmail.com>
2024-08-27 01:13:13 +0300

    edit aladhan api url

--- src/services/prayerTimeApi.js
+++ src/services/prayerTimeApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const prayerApi = createApi({
   reducerPath: 'prayerApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://api.aladhan.com/v1/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.aladhan.com/v1/' }),
   endpoints: (builder) => ({
     getPrayerTimesByCity: builder.query({
       query: ({ city, country }) => `timingsByCity?city=${city}&country=${country}&method=5`,
